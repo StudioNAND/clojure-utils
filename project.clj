a8884e1d4df4afe9d5a2747c8c010171830cd9e7
Stephan Thiel <stephan@nand.io>
2016-06-17 14:33:33 +0200

    add dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject nand "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A selection of helper utils"
+  :url "https://github.com/StudioNAND/nand-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "2.2.0"]
+                 [cheshire "5.6.1"]
+                 [clojure-csv/clojure-csv "2.0.1"]
+                 [clj-time "0.11.0"]])
