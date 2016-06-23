b20f56c29ec29ebe70e2e9a83153b97200e3f539
Stephan Thiel <stephan@nand.io>
2016-06-24 01:28:13 +0200

    add uuid util to core

--- src/nand/core.clj
+++ src/nand/core.clj
@@ -1,6 +1,6 @@
 (ns nand.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn uuid
+  "Retrieve a type 4 (pseudo randomly generated) UUID. The UUID is generated using a cryptographically strong pseudo random number generator." 
+  []
+  (str (java.util.UUID/randomUUID)))
