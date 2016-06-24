7c113f454bbb1c162a8ce8e4b4d68f515ce8e772
Stephan Thiel <stephan@nand.io>
2016-06-24 02:27:05 +0200

    updated description & url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject io.nand/utils "0.1.0-SNAPSHOT"
-  :description "A learning project more than a collection of professional tools..."
-  :url "https://github.com/StudioNAND/nand-clj"
+  :description "A learning project really..."
+  :url "https://github.com/StudioNAND/clojure-utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
