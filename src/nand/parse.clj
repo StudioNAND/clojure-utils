2e1c4425dfbffd2c947c5bf2fece385a0e39991b
Stephan Thiel <stephan@nand.io>
2016-11-09 06:45:33 +0100

    parse dates using java.sql.Timestamp

--- src/nand/parse.clj
+++ src/nand/parse.clj
@@ -4,8 +4,13 @@
 (defn parse-int [s]
   (try (Integer/parseInt s) (catch NumberFormatException e nil)))
 
-(defn parse-date [s]
-  (try (f/parse (f/formatters :date-time-no-ms) s) (catch Exception e nil)))
+(defn parse-date
+  "
+  Converts a String object in JDBC timestamp escape format to a Timestamp value.
+  https://docs.oracle.com/javase/7/docs/api/java/sql/Timestamp.html
+  "
+  [s]
+  (java.sql.Timestamp/valueOf s))
 
 (defn encode-date [s]
   (f/unparse (f/formatters :date-time-no-ms) s))
